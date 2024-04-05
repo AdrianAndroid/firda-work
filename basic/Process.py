5b8f9be36e3ef2779cc625c6e61bedc4f9623a59
zhaojian <18310579837@163.com>
2024-04-05 23:13:39 +0800

    封装功能函数

---
+++ basic/Process.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import frida
+import codecs
+
+
+class Process:
+    apkName = ''
+    jsPath = ''
+
+    def __init__(self, apkName, jsPath):
+        self.apkName = apkName
+        self.jsPath = jsPath
+
+    def start(self):
+        print(self.apkName, self.jsPath)
+        process = frida.get_remote_device().attach(self.apkName)
+        if not os.path.isfile(self.jsPath):
+            raise TypeError(self.jsPath + ' does not exists')
+        with codecs.open(self.jsPath, 'r', 'UTF-8') as file:
+            js_code = file.read()
+        script = process.create_script(js_code)
+
+        def message(msg, data):
+            if msg["type"] == 'send':
+                print("[*] {0}".format(msg['payload']))
+            else:
+                print(msg)
+
+        script.on('message', message)
+        script.load()
+        sys.stdin.read()
+
+
+# frida使用非标准端口
+# /data/local/tmp # ./fs_12.7.22_arm64 -l 127.0.0.1:31928  默认端口: 27046
+# process = frida.get_device_manager().add_remote_device('127.0.0.1:31928').attach('FridaApp')
+# if not os.path.isfile('./JavaHook.js'):
+#     raise TypeError("./JavaHook.js does not exist")
+# with codecs.open('./JavaHook.js', 'r', 'UTF-8') as file:
+#     js_code = file.read()
+# script = process.create_script(js_code)
+# script.on("message", message)
+# script.load()
+# sys.stdin.read()
+
+
+class IPProcess(Process):
+    __remoteIp = ''
+
+    def __init__(self, apkName, jsPath, remoteIp):
+        Process.__init__(self, apkName, jsPath)
+        self.__remoteIp = remoteIp
+
+    def start(self):
+        print(self.apkName, self.jsPath, self.__remoteIp)
+        process = frida.get_device_manager().add_remote_device(self.__remoteIp).attach(self.apkName)
+        if not os.path.isfile(self.jsPath):
+            raise TypeError(self.jsPath + ' does not exists')
+        with codecs.open(self.jsPath, 'r', 'UTF-8') as file:
+            js_code = file.read()
+        script = process.create_script(js_code)
+
+        def message(msg, data):
+            if msg["type"] == 'send':
+                print("[*] {0}".format(msg['payload']))
+            else:
+                print(msg)
+
+        script.on('message', message)
+        script.load()
+        sys.stdin.read()
--- frida_demo/JavaHook.py
+++ frida_demo/JavaHook.py
@@ -1,30 +1,61 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import frida
-import codecs
+# import os
+# import sys
+# import frida
+# import codecs
+#
+#
+# class Process2:
+#     __apkName = ''
+#     __jsPath = ''
+#
+#     def __init__(self, apkName, jsPath):
+#         self.__apkName = apkName
+#         self.__jsPath = jsPath
+#
+#     def start(self):
+#         print(self.__apkName, self.__jsPath)
+#         process = frida.get_remote_device().attach(self.__apkName)
+#         if not os.path.isfile(self.__jsPath):
+#             raise TypeError(self.__jsPath + ' does not exists')
+#         with codecs.open(self.__jsPath, 'r', 'UTF-8') as file:
+#             js_code = file.read()
+#         script = process.create_script(js_code)
+#
+#         def message(msg, data):
+#             if msg["type"] == 'send':
+#                 print("[*] {0}".format(msg['payload']))
+#             else:
+#                 print(msg)
+#
+#         script.on('message', message)
+#         script.load()
+#         sys.stdin.read()
+from basic.Process import IPProcess
 
+IPProcess("NDKDemo", "./JavaHook.js", "127.0.0.1:31928").start()
 
-def message(message, data):
-    if message["type"] == 'send':
-        print("[*] {0}".format(message['payload']))
-    else:
-        print(message)
-
-process = frida.get_remote_device().attach('NDKDemo')
-if not os.path.isfile('./JavaHook.js'):
-    raise TypeError("./JavaHook.js does not exist")
-with codecs.open('./JavaHook.js', 'r', 'UTF-8') as file:
-    js_code = file.read()
-script = process.create_script(js_code)
-script.on("message", message)
-script.load()
-# script.exports.test()
-# script.exports.test()
-# script.exports.test()
-# script.exports.test()
-sys.stdin.read()
+# def message(msg, data):
+#     if msg["type"] == 'send':
+#         print("[*] {0}".format(msg['payload']))
+#     else:
+#         print(msg)
+#
+#
+# process = frida.get_remote_device().attach('NDKDemo')
+# if not os.path.isfile('./JavaHook.js'):
+#     raise TypeError("./JavaHook.js does not exist")
+# with codecs.open('./JavaHook.js', 'r', 'UTF-8') as file:
+#     js_code = file.read()
+# script = process.create_script(js_code)
+# script.on("message", message)
+# script.load()
+# # script.exports.test()
+# # script.exports.test()
+# # script.exports.test()
+# # script.exports.test()
+# sys.stdin.read()
 
 # frida使用非标准端口
 # /data/local/tmp # ./fs_12.7.22_arm64 -l 127.0.0.1:31928  默认端口: 27046
@@ -37,4 +68,3 @@ sys.stdin.read()
 # script.on("message", message)
 # script.load()
 # sys.stdin.read()
-
